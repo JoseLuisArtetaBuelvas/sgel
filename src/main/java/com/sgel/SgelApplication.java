89e2ae462c2c42775e8075a74df7c7df0ea8eb1b
José Luis Arteta Buelvas <jartetab@unicartagena.edu.co>
2026-04-10 03:20:57 -0500

    feat: proyecto Maven + Spring Boot inicializado

---
+++ src/main/java/com/sgel/SgelApplication.java
@@ -0,0 +1,12 @@
+package com.sgel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgelApplication.class, args);
+    }
+}
